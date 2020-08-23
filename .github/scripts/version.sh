e98aa618a7bc22385d1bfdb4d2910cd99d757ad7
Nightmayr <umayr23@gmail.com>
2020-08-23 22:53:18 +0100

    added citra type and version to app titlebar

--- .github/scripts/version.sh
+++ .github/scripts/version.sh
@@ -19,17 +19,21 @@ if [ $CURRENT_NIGHTLY_VERSION_TAG != $LATEST_NIGHTLY_VERSION_TAG ] || [ $CURRENT
         echo "citra-canary:: github: $LATEST_CANARY_VERSION_TAG snap: $CURRENT_CANARY_VERSION_TAG"
         echo "updating snapcraft.yaml with new versions"
         yq w -i snap/snapcraft.yaml parts.citra-nightly.source-tag $LATEST_NIGHTLY_VERSION_TAG
+        yq w -i snap/snapcraft.yaml parts.citra-nightly.build-environment[2].TRAVIS_TAG $LATEST_NIGHTLY_VERSION_TAG
         yq w -i snap/snapcraft.yaml parts.citra-canary.source-tag $LATEST_CANARY_VERSION_TAG
+        yq w -i snap/snapcraft.yaml parts.citra-canary.build-environment[2].TRAVIS_TAG $LATEST_CANARY_VERSION_TAG
         yq w -i snap/snapcraft.yaml version C$LATEST_CANARY_VERSION-N$LATEST_NIGHTLY_VERSION
     elif [ $CURRENT_NIGHTLY_VERSION_TAG != $LATEST_NIGHTLY_VERSION_TAG ]; then
         echo "citra-nightly versions don't match, github: $LATEST_NIGHTLY_VERSION_TAG snap: $CURRENT_NIGHTLY_VERSION_TAG"
         echo "updating snapcraft.yaml with new nightly version"
         yq w -i snap/snapcraft.yaml parts.citra-nightly.source-tag $LATEST_NIGHTLY_VERSION_TAG
+        yq w -i snap/snapcraft.yaml parts.citra-nightly.build-environment[2].TRAVIS_TAG $LATEST_NIGHTLY_VERSION_TAG
         yq w -i snap/snapcraft.yaml version C$CURRENT_CANARY_VERSION_SNAP-N$LATEST_NIGHTLY_VERSION
     else
         echo "citra-canary versions don't match, github: $LATEST_CANARY_VERSION_TAG snap: $CURRENT_CANARY_VERSION_TAG"
         echo "updating snapcraft.yaml with new canary version"
         yq w -i snap/snapcraft.yaml parts.citra-canary.source-tag $LATEST_CANARY_VERSION_TAG
+        yq w -i snap/snapcraft.yaml parts.citra-canary.build-environment[2].TRAVIS_TAG $LATEST_CANARY_VERSION_TAG
         yq w -i snap/snapcraft.yaml version C$LATEST_CANARY_VERSION-N$CURRENT_NIGHTLY_VERSION_SNAP
     fi
     
